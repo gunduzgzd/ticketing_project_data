5e9f74af83823222e585a824388e628cd576329f
gozdegunduz <gunduzgzd@gmail.com>
2024-05-15 15:57:09 -0400

    new repository for data section has been started

---
+++ src/main/java/com/example/converter/UserDtoConverter.java
@@ -0,0 +1,31 @@
+package com.example.converter;
+
+import com.example.dto.UserDTO;
+//import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+/*
+@Component
+//@ConfigurationPropertiesBinding
+public class UserDtoConverter implements Converter<String, UserDTO> {
+
+    UserService;
+
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return userService.findById(source);
+
+    }
+
+ */
+
+}
