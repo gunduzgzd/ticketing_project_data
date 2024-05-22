16a83e0e5173fb864dc11b311ff3fcf347e43169
gozdegunduz <gunduzgzd@gmail.com>
2024-05-22 19:18:49 -0400

    html button bugs has been fixed, controller class validation annotations added

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -41,7 +41,7 @@ public class TaskController {
 
 
     @PostMapping("/create")
-    public String insertTask(@ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
 
